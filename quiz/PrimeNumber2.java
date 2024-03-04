de2af451de68d6cb92a8de8d31c3681f06cf80b5
jwkim97211 <160681245+jwkim97211@users.noreply.github.com>
2024-03-04 23:20:38 +0900

    Update PrimeNumber2.java

--- quiz/PrimeNumber2.java
+++ quiz/PrimeNumber2.java
@@ -5,23 +5,27 @@ import java.util.Scanner;
 public class PrimeNumber2 {
 	public void search(int s, int e) {
 		boolean flag = true;
-		for (int i = s; i < e; i++) {
-			for(int j=2;j<Math.sqrt(i);j++) {
-				if() {
-					
+		for (int i = s; i <= e; i++) {
+			flag = true;
+			for (int k = 2; k < Math.sqrt(i); k++) {
+				if (i % k == 0) {
+					flag = false;
+					break;
 				}
 			}
+			if (flag == true)
+				System.out.println(i);
 		}
 	}
 
 	public static void main(String[] args) {
-		SearchPrime2 sp = new SearchPrime2();
+		PrimeNumber2 sp = new PrimeNumber2();
 		Scanner sc = new Scanner(System.in);
 		System.out.print("자릿수 : ");
 		int num = sc.nextInt();
 		int s = (int) Math.pow(10.0, (double) (num - 1));
 		int e = (int) Math.pow(10.0, (double) (num)) - 1;
 		sp.search(s, e);
-	}
 
+	}
 }
