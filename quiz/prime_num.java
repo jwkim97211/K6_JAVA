562398f42eb31b33c21fd7580fc9a40e9204fb84
jwkim97211 <160681245+jwkim97211@users.noreply.github.com>
2024-03-04 17:47:54 +0900

    Add files via upload

--- quiz/prime_num.java
+++ quiz/prime_num.java
@@ -4,23 +4,27 @@ import java.util.Scanner;
 
 public class prime_num {
 
-		public static void main(String[] args) {
+	public static void main(String[] args) {
 			Scanner sc = new Scanner(System.in);
 			System.out.print("숫자 : ");
 			int num = sc.nextInt();
 			
-			boolean flag = true;
-			if(num==2) {
-				System.out.println("소수입니다.");
-			} else {
-				for(int i=2; i<num; i++) {
-					if(num%i==0) {
-						flag=false;		
-					}
-				}
+			boolean flag= true;
+		      if (num <= 1) {
+		            flag = false;
+		        } else {
+		            for (int i = 2; i <= Math.sqrt(num); i++) {
+		                if (num % i == 0) {
+		                	flag = false;
+		                    break;
+		                }
+		            }
+		        }
+			
+
 				System.out.println(flag==true?"소수입니다.":"소수가 아닙니다.");
 			}
 
-	}
-
 }
+
+
